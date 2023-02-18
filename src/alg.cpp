149b429c155befdfdc0a40c6b468ffb707ac6fcb
Kirill Kashkirov <124998794+Kashkirov@users.noreply.github.com>
2023-02-18 19:52:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,78 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+for (uint64_t t = 2; t < value; t++) {
+        if (value % t == 0) {
+            return false;
+            
+
+        }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+uint64_t count = 0;
+    if (!n) {
+        return 0;
+    }
+    bool flag = false;
+    for (uint64_t i = 2; i > 0; i++) {
+        flag = false;
+        for (uint64_t t = 2; t < i; t++) {
+            if (i % t == 0) {
+                flag = true;
+                break;
+            }
+        }
+        if (flag == true) {
+            continue;
+        }
+        else {
+            count++;
+        }
+        if (count == n) {
+            return i;
+        }
+    }
 }
-
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+bool flag = false;
+    for (uint64_t i = value+1; i > 0; i++) {
+        flag = false;
+        for (uint64_t t = 2; t < i; t++) {
+            if (i % t == 0) {
+                flag = true;
+                break;
+            }
+        }
+        if (flag == true) {
+            continue;
+        }
+        else {
+            return i;
+        }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t count = 0;
+    if (!hbound) {
+        return 0;
+    }
+    bool flag = false;
+    //int l_count = 0;
+    for(uint64_t i=2 ;i<hbound;i++) {
+        flag = false;
+        for (uint64_t t = 2; t < i; t++) {
+            if (i % t == 0) {
+                flag = true;
+                break;
+            }
+        }
+        if (flag == false) {
+            count += i;
+        }
+    }
+    return count;
 }
